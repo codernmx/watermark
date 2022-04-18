b7ae76c710b34e132fe9c47e072bdfd8e25c3c53
codernmx <2507128400@qq.com>
2022-04-18 21:39:44 +0800

    个人中心

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
   微信:19923756596
 */
 App({
-    onLaunch: function() {},
+    onLaunch: function() {
+        wx.cloud.init({
+            env: 'cloud1-6g5r1b2z227ba3e1', //填上你的云开发环境id
+            traceUser: true,
+        })
+    },
     globalData: {
         userInfo: null
     }
